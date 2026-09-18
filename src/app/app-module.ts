37980c59b6fc5f535a0e4f8af5ea685a69f67cd9
PedroAP <pamericoparussolo@gmail.com>
2026-09-17 22:22:43 -0300

    v3

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -6,13 +6,17 @@ import { App } from './app';
 import { Header } from './template/header/header';
 import { Footer } from './template/footer/footer';
 import { Home } from './components/home/home';
+import { Listarprodutos } from './components/produtos/listarprodutos/listarprodutos';
+import { Cadastrarproduto } from './components/produtos/cadastrarproduto/cadastrarproduto';
 
 @NgModule({
   declarations: [
     App,
     Header,
     Footer,
-    Home
+    Home,
+    Listarprodutos,
+    Cadastrarproduto
   ],
   imports: [
     BrowserModule,
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -7,5 +7,54 @@ import { Component } from '@angular/core';
   styleUrl: './home.css',
 })
 export class Home {
+  nomeProduto: string = "Curso de Angular";
+  anuncio: string = "O " + this.nomeProduto + " está em promoção";
+  idProduto: number = 123;
+  precoProduto: number = 2.59;
+  promocao: boolean = true;
+
+
+  testeVariaveis(): void {
+    console.log(`Nome Produto: ${this.nomeProduto}`);
+    console.log(`${this.anuncio} está em promoção`);
+    console.log(`ID: ${this.idProduto}`);
+    console.log(`Preço: ${this.precoProduto}`);
+    console.log(`Promoção: ${this.promocao}`);
+
+
+    // escopo de variaveis dentro do código 
+    var variavel1;
+    let variavel2;
+    const constante3 = 3;
+
+    // var
+    var idade = 10;
+
+    function imprimeIdade() {
+      var idade = 50;
+      console.log("Minha idade é: ", idade);
+    }
+    imprimeIdade();
+
+    //let
+    function imprimeIdade2() {
+      for (let idade = 30; idade <= 40; idade++) {
+        console.log(`Idade dentro do for: ${idade}`)
+      }
+      console.log(`Idade fora do for: ${idade}`)
+    }
+    imprimeIdade2();
+
+    let a = 5;
+    var b = 10;
+    if (a = 5) {
+      let a = 4;
+      let b = 1;
+      console.log('Variável a dentro do if: ', a);
+      console.log('Variável b dentro do if: ', b);
+    }
+    console.log('Variável a fora do if: ', a);
+    console.log('Variável b fora do if: ', b);
+  }
 
 }
